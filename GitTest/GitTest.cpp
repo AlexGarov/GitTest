44689c427a2df63495b630cef59a865cf70d3d1d
AlexGarov <58984168+AlexGarov@users.noreply.github.com>
2020-11-02 14:49:48 +0200

    sdfdfsf

--- GitTest/GitTest.cpp
+++ GitTest/GitTest.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello World Alex Vika!\n";
+    std::cout << "Hello World Alex Vikadsf!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
