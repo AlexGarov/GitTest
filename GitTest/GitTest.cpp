b8fd0d6a8f77be28055c01a41650467ecc1d20ca
AlexGarov <58984168+AlexGarov@users.noreply.github.com>
2020-11-02 13:46:59 +0200

    Vika

--- GitTest/GitTest.cpp
+++ GitTest/GitTest.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello World Alex, Kola!\n";
+    std::cout << "Hello World Alex, Kola, Vika!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
