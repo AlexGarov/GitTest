32862e910bd9b72c7f5ebaa3e2ecd8e4a243e99e
korchak oleksiy <korchakoleksiy@outlook.com>
2020-11-27 12:17:26 +0200

    put class

--- GitTest/GitTest.cpp
+++ GitTest/GitTest.cpp
@@ -1,8 +1,47 @@
 
 #include <iostream>
+using namespace std;
+class A
+{
+public:
+    
+    A(int a)
+    {
+        i = a;
+    }
+    int i;
+};
+class B:public A
+{
+public:
+   // using A::A;
+   // using A::i;
+   /* B(int i):A(i)
+    {
 
+    }*/
+   
+       void Bind()
+       {
+           cout << " Bind(int i)::B= "<<i<<endl;
+
+       }
+       
+};
+class App
+{
+public:
+    App(int i)
+    {
+        b = new B(i);
+        b->Bind();
+    };
+    B *b;
+};
 int main()
 {
+    App app(8);
+
     std::cout << "alex 12:31";
 }
 
