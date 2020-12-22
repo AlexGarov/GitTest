843e40b2d96544d09a4bc39d09d3b058f08eb5b2
korchak oleksiy <korchakoleksiy@outlook.com>
2020-12-22 13:02:46 +0200

    using std::map

--- GitTest/GitTest.cpp
+++ GitTest/GitTest.cpp
@@ -1,32 +1,29 @@
 #include<iostream>
 #include<vector>
-template<char T>
-int calculete(int arg1, int arg2);
-template<>
-int calculete<'+'>(int arg1, int arg2)
+#include<map>
+int Plus(int arg1, int arg2)
 {
 	return arg1 + arg2;
 }
-template<>
-int calculete<'-'>(int arg1, int arg2)
+int Minus(int arg1, int arg2)
 {
 	return arg1 - arg2;
-}
-template<>
-int calculete<'*'>(int arg1, int arg2)
-{
-	return arg1 * arg2;
-}
-template<>
-int calculete<'/'>(int arg1, int arg2)
-{
-	return arg1 / arg2;
 }
 
 int main()
 {
+
+	std::map<char, int(*)(int, int)> cal;
+	cal.insert(std::pair<char, int(*)(int, int)>('+', Plus));
+	cal.insert(std::pair<char, int(*)(int, int)>('-', Minus));
+
+	auto it = cal.find('+');
+	if (it != cal.end())
+		std::cout << it->second(6,8);     
+	else std::cout << "str not found\n";
+
+
 	
-	auto i = calculete<'+'>(2, 7);
 	int stop = 0;
 	return 0;
 }
